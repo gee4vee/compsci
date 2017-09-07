00bb2dc3937723f58c008eef0561ba75e4cc490b
gee4vee <gee4vee@gabev-megapc>
2017-09-06 20:02:05 -0700

    Add some character to the dining philosopher problem.

--- Practice/src/DiningPhilosophers.java
+++ Practice/src/DiningPhilosophers.java
@@ -5,8 +5,18 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * A solution to the classic problem using a Lock and Condition.
+ * 
+ * @author Gabriel Valencia, gee4vee@me.com
+ */
 public class DiningPhilosophers {
 	
+	/**
+	 * A philosopher just thinks, eats, and then dies.
+	 * 
+	 * @author Gabriel Valencia, gee4vee@me.com
+	 */
 	private static class Philosopher implements Runnable {
 		
 		private static Random rand = new Random();
@@ -27,6 +37,9 @@ public class DiningPhilosophers {
 			this.fast = fast;
 		}
 
+		/**
+		 * Thinks and eats repeatedly for a random lifespan until he/she dies.
+		 */
 		@Override
 		public void run() {
 			long start = System.currentTimeMillis();
@@ -46,9 +59,12 @@ public class DiningPhilosophers {
 			}
 		}
 		
+		/**
+		 * The philosopher pauses for some random length of time to contemplate the big questions of life.
+		 */
 		public void think() {
+			System.out.println("Philosopher #" + this.id + ": I wonder...");
 			try {
-				System.out.println("Philosopher " + this.id + " is thinking...");
 				if (!this.fast) {
 					Thread.sleep(rand.nextInt(21000) + 1);
 				}
@@ -57,11 +73,16 @@ public class DiningPhilosophers {
 			}
 		}
 		
+		/**
+		 * The philosopher first indicates that he/she is hungry and then attempts to pick up both chopsticks. He/she will wait 
+		 * until both are available. When they are, he/she picks them both up and eats for some time. Afterwards, the 
+		 * chopsticks are both put down.
+		 */
 		public void eat() {
-			System.out.println("Philosopher " + this.id + " is hungry!");
+			System.out.println("Philosopher #" + this.id + ": All this thinking has made me HUNGRY!");
 			this.leftChopstick.pickUp();
 			this.rightChopstick.pickUp();
-			System.out.println("Philosopher " + this.id + " is eating...");
+			System.out.println("Philosopher #" + this.id + ": This food is good!");
 			try {
 				if (!this.fast) {
 					Thread.sleep(10000L);
@@ -69,18 +90,26 @@ public class DiningPhilosophers {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.println("Philosopher " + this.id + " finished eating!");
+			System.out.println("Philosopher #" + this.id + ": I'm full for now!");
 			this.leftChopstick.putDown();
 			this.rightChopstick.putDown();
 		}
 		
+		/**
+		 * The philosopher dies and finally stops thinking and eating.
+		 */
 		public void die() {
 			this.alive = false;
-			System.out.println("Philosopher " + this.id + " has DIED! :-(");
+			System.out.println("Philosopher #" + this.id + ": Goodbye, cruel world! (✖╭╮✖)");
 		}
 		
 	}
 	
+	/**
+	 * Represents the classic eating utensil. Can be picked up and put down.
+	 * 
+	 * @author Gabriel Valencia, gee4vee@me.com
+	 */
 	private static class Chopstick {
 		
 		private final Lock chopstickLock = new ReentrantLock();
@@ -97,26 +126,35 @@ public class DiningPhilosophers {
 			return this.id;
 		}
 		
+		/**
+		 * Called by a philosopher to reserve a chopstick before eating.
+		 */
 		public void pickUp() {
 			this.chopstickLock.lock();
 			try {
 				if (this.inUse) {
 					try {
+						// chopstick is in use. the philosopher will wait (releasing the lock) until signaled in putDown().
 						this.chopstickAvailable.await();
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
+				// philosopher will reserve this chopstick.
 				this.inUse = true;
 			} finally {
 				this.chopstickLock.unlock();
 			}
 		}
 		
+		/**
+		 * Called by a philosopher to release a chopstick after eating.
+		 */
 		public void putDown() {
 			this.chopstickLock.lock();
 			try {
 				this.inUse = false;
+				// a philosopher waiting in pickUp() will be chosen at random to continue.
 				this.chopstickAvailable.signalAll();
 			} finally {
 				this.chopstickLock.unlock();
@@ -124,9 +162,18 @@ public class DiningPhilosophers {
 		}
 	}
 	
+	/**
+	 * Represents a circular table. The circular configuration promotes discussion amongst philosophers.
+	 * 
+	 * @author Gabriel Valencia, gee4vee@me.com
+	 */
 	private static class CircularTable {
 		private List<Philosopher> diners = new ArrayList<>();
 		
+		/**
+		 * @param fast If <code>true</code>, philosophers won't pause much, which is a more rigorous test for deadlocks. 
+		 * Use <code>false</code> to follow the philosophers' actions more easily.
+		 */
 		public CircularTable(boolean fast) {
 			Chopstick c1 = new Chopstick(1);
 			Chopstick c2 = new Chopstick(2);
@@ -161,6 +208,9 @@ public class DiningPhilosophers {
 			p5.leftChopstick = c1;
 		}
 		
+		/**
+		 * Begins the lifecycle of all philosophers dining at this table.
+		 */
 		public void start() {
 			for (Philosopher diner : this.diners) {
 				Thread pThread = new Thread(diner);
@@ -168,6 +218,9 @@ public class DiningPhilosophers {
 			}
 		}
 		
+		/**
+		 * Immediately kills all philosophers dining at this table.
+		 */
 		public void stop() {
 			for (Philosopher diner : this.diners) {
 				diner.die();
@@ -175,8 +228,18 @@ public class DiningPhilosophers {
 		}
 	}
 
+	/**
+	 * Runs the dining philosopher simulation.
+	 * 
+	 * @param args Pass in <code>true</code> to enable fast mode for more rigorous deadlock testing.
+	 */
 	public static void main(String[] args) {
-		CircularTable table = new CircularTable(true);
+		boolean fast = false;
+		if (args.length > 0) {
+			String arg = args[0];
+			fast = Boolean.parseBoolean(arg);
+		}
+		CircularTable table = new CircularTable(fast);
 		table.start();
 	}
 
